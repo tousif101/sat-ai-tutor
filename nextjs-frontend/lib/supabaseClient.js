4c36a77046780112b5a1d016d8dbd5902b42c9d2
tousif101 <tousif101@gmail.com>
2025-04-28 19:01:23 -0400

    remove supabase url and key

--- nextjs-frontend/lib/supabaseClient.js
+++ nextjs-frontend/lib/supabaseClient.js
@@ -3,9 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Key:", supabaseAnonKey);
-
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables! Check .env.local");
 }
